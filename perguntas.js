9d167b5b97a413e878a0da3524c5239f8f64acd9
drospiy <drospiy@gmail.com>
2024-10-30 22:49:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,48 @@
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo?',
+    'Ásia'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o rio mais longo do planeta?',
+    'O rio Nilo (ou o rio Amazonas, dependendo da medição)'
+
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são os cinco oceanos do mundo?',
+    'Oceano Pacífico, Oceano Atlântico, Oceano Índico, Oceano Antártico (ou Austral) e Oceano Ártico'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é um meridiano e como ele é utilizado na localização geográfica',
+    'Um meridiano é uma linha imaginária que vai do Polo Norte ao Polo Sul. Eles são usados para medir a longitude'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França?',
+    'Paris.'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são os principais biomas terrestres e onde eles se localizam?',
+    'Floresta tropical (próxima ao equador), savana (em regiões tropicais), deserto (em regiões áridas), tundra (próxima aos polos), e floresta temperada (em regiões temperadas)'
+)
+
+criaCartao(
+    'Geografia',
+    'O que caracteriza uma região montanhosa?',
+    'Uma região montanhosa é caracterizada por elevações significativas no terreno, com picos e vales, geralmente com altitudes superiores a 300 metros'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a diferença entre clima e tempo atmosférico?',
+    'Clima refere-se às condições atmosféricas médias de uma região ao longo de um longo período, enquanto tempo atmosférico refere-se às condições atmosféricas em um momento específico'
+)
